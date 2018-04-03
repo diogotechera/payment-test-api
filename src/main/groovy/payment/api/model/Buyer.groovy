757ef12cb9906ff258d50f736b3ce7da31913de1
Diogo Techera <diogo@unovation.com.br>
2018-04-02 22:25:12 -0300

    field validations

--- src/main/groovy/payment/api/model/Buyer.groovy
+++ src/main/groovy/payment/api/model/Buyer.groovy
@@ -2,7 +2,9 @@ package payment.api.model
 
 import javax.persistence.Column
 import javax.persistence.Embeddable
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 @Embeddable
 class Buyer {
@@ -11,11 +13,13 @@ class Buyer {
     @Column(name= 'buyer_name')
     String name
 
+    @Email
     @NotNull
     @Column(name= 'buyer_email')
     String email
 
     @NotNull
+    @Pattern(regexp = '^[0-9]{11}')
     @Column(name= 'buyer_cpf')
     String cpf
 }
